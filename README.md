e99df0bcf49376abaf02f510e26e820e2b5d3c88
onurkaymak <onurkaymak@outlook.com>
2023-08-15 22:49:17 -0700

    add wordCounter function and first test

---
+++ README.md
@@ -0,0 +1,8 @@
+Describe: wordCounter()
+
+TEST #1
+Test: "It should return 1 if a passage has just one word."
+Code:
+const text = "hello";
+wordCounter(text);
+Expected Output: 1
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-window.addEventListener("load", function () {
+// Business Logic
 
+function wordCounter(text) {
+    let wordCount = 0;
+    const textArray = text.split(" ");
+    textArray.forEach(function (word) {
+        wordCount++;
+    });
+    return wordCount;
+}
+
+
+window.addEventListener("load", function () {
+    wordCounter()
 });
