4377a94169f6237a547d79c32bfd7746f53403cb
onurkaymak <onurkaymak@outlook.com>
2023-08-17 08:08:58 -0700

    numberOfOccurrencesInText function test#7, passing.

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,9 @@ function wordCounter(text) {
 
 
 function numberOfOccurrencesInText(word, text) {
+    if (word.trim().length === 0) {
+        return 0;
+    }
     const textArray = text.split(" ");
     let wordCount = 0;
     textArray.forEach(function (element) {
